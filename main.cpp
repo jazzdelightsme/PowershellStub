5a730c482a878c5f9fa85adc2d3032ded8558c02
Dan Thompson (SBS) <danthom@microsoft.com>
2026-01-19 08:47:04 -0800

    -NonInteractive, more usage info, PrettyPrint option, some comments and cleanup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //
-// PowershellStub: a very small EXE that just launches Windows PowerShell (passing
-// arguments along).
+// PowershellStub: a very small EXE that launches Windows PowerShell to download, verify,
+// and run a script.
 //
 
 #include <Windows.h>
@@ -38,7 +38,6 @@ inline char8_t* SkipUntilNotWhitespace( char8_t* str )
     return str;
 } // end SkipUntilNotWhitespace()
 
-
 // Returns a pointer to the part of the current process command line immediately after the
 // EXE.
 inline char8_t* FindTheRestOfTheCommandLine()
@@ -70,13 +69,9 @@ inline char8_t* CopyStr( char8_t* dest, const char8_t* src, const char8_t* pastD
     return dest;
 } // end CopyStr()
 
-HANDLE g_hStdOut = 0;
-
 #define _countof(_Array) ((sizeof(_Array) / sizeof(_Array[0])))
 
-// TODO: does this really evaluate at compile-time, or do I need to switch to a recursive
-// method?
-constexpr DWORD MyWcslen( const char8_t* s )
+constexpr DWORD MyStrLen( const char8_t* s )
 {
     DWORD cch = 0;
     while( *s )
@@ -85,12 +80,14 @@ constexpr DWORD MyWcslen( const char8_t* s )
         s++;
     }
     return cch;
-}
+} // end MyStrLen()
+
+HANDLE g_hStdOut = 0;
 
 void Print( const char8_t* s )
 {
-    WriteConsoleA( g_hStdOut, s, MyWcslen( s ), nullptr, nullptr );
-} // end Print()
+    WriteConsoleA( g_hStdOut, s, MyStrLen( s ), nullptr, nullptr );
+}
 
 int MyStrCmpI( const char8_t* s1, const char8_t* s2 )
 {
@@ -107,30 +104,139 @@ bool _HandleUsageRequest( const char8_t* installMode )
         (0 == MyStrCmpI( installMode, c_Help2 )) ||
         (0 == MyStrCmpI( installMode, c_Help3 )) )
     {
-        Print( u8"\nUsage: PowershellStub.exe [WhatIf] <InstallMode> <ScriptURL> <ExpectedScriptHash> [<OptionalArgs>]\n"
+        Print( u8"\nUsage: PowershellStub.exe [WhatIf|WhatIfRaw] <InstallMode> <ScriptURL> <ExpectedScriptHash> [<OptionalArgs>]\n"
                u8"\n"
-               u8"This program is intended to be used as an \"installer\" EXE for winget, where the actual\n"
-               u8"install logic is implemented in a downloadable PowerShell script. It launches powershell.exe\n"
-               u8"with a command line that downloads the script, verifies its hash, verifies its signature\n"
-               u8"(if any) and then runs it.\n"
+               u8"This program is intended to be used as an \"installer\" EXE for winget, where the actual install logic is\n"
+               u8"implemented in a downloadable PowerShell script. It launches powershell.exe with a command line that contains\n"
+               u8"a wrapper script, which downloads the install script, verifies its hash, verifies its signature (if any), and\n"
+               u8"then runs it.\n"
                u8"\n"
-               u8"  WhatIf: (optional) if specified, just prints the command line that would be used.\n"
+               u8"    WhatIf | WhatIfRaw: (optional) if specified, just prints the command line that would be used.\n"
                u8"\n"
-               u8"  InstallMode: one of Interactive, Silent, or SilentWithProgress.\n"
+               u8"    InstallMode: one of Interactive, Silent, or SilentWithProgress.\n"
                u8"\n"
-               u8"  ScriptURL: URL from which to download the PowerShell script to run.\n"
+               u8"    ScriptURL: URL from which to download the PowerShell script to run.\n"
                u8"\n"
-               u8"  ExpectedScriptHash: SHA256 hash of the script, in hex.\n"
+               u8"    ExpectedScriptHash: SHA256 hash of the install script, in hex.\n"
                u8"\n"
-               u8"  OptionalArgs: optional arguments to pass along to the script.\n\n"
+               u8"    OptionalArgs: optional arguments to pass along to the script.\n"
+               u8"\n"
+               u8"Details about how the install script is executed (use WhatIf to see the wrapper script):\n"
+               u8"\n"
+               u8"The script is executed with StrictMode on, RemoteSigned execution policy, and -NoProfile.\n"
+               u8"\n"
+               u8"The value of the environment variable $env:_POWERSHELL_STUB_TEST_URL_SUFFIX is appended to the ScriptUrl.\n"
+               u8"This is handy for testing a new version of the install script without having to update the winget package.\n"
+               u8"\n"
+               u8"The winget install mode is passed as a switch parameter to the script (e.g. -Silent), followed by the optional\n"
+               u8"parameters, followed by -EA Stop (all errors are treated as terminating errors).\n"
+               u8"\n"
+               u8"The downloaded install script does not have to be signed, but if it is, the signature must be valid.\n"
+               u8"\n"
+               u8"If an error occurs in Interactive mode, the wrapper script pauses so you can read the error. If you enter 'd'\n"
+               u8"at the prompt, you'll enter a nested shell so you can poke around more (run 'exit' to quit the nested shell).\n"
+               u8"\n"
+               u8"PowerShellStub.exe returns the return code of the powershell.exe process. The exit code for an uncaught\n"
+               u8"exception is 1.\n"
+               u8"\n"
              );
         return true;
     }
     return false;
 } // end _HandleUsageRequest()
 
+bool StartsWith( const char8_t* str, const char8_t* prefix )
+{
+    while( *prefix )
+    {
+        if( *str != *prefix )
+        {
+            return false;
+        }
+        str++;
+        prefix++;
+    }
+    return true;
+} // end StartsWith()
+
+// This is an EXTREMELY basic algorithm for printing out the PowerShell script in a
+// somewhat readable way.
+//
+// N.B. Getting good results depends on careful use of whitespace in the script!
+//
+// If an opening brace is surrounded by space, or a closing brace is preceded by a space,
+// that's the signal that we want the brace on its own line; else it's left alone (for
+// ScriptBlocks).
+//
+// Semicolons are turned into newlines.
+//
+// The command line escaping for double quotes is removed.
+void PrettyPrint( char8_t* s )
+{
+    int indent = 4;
+
+    auto printIndent = [&]() {
+        for( int i = 0; i < indent; i++ )
+        {
+            Print( u8" " );
+        }
+    };
+
+    auto skipSpaces = [&]() {
+        while( *s == ' ' )
+        {
+            s++;
+        }
+    };
+
+    while( *s )
+    {
+        if( *s == ';' )
+        {
+            Print( u8"\n" );
+            printIndent();
+            s += 1;
+            skipSpaces();
+        }
+        else if( StartsWith( s, u8" { " ) )
+        {
+            Print( u8"\n" );
+            printIndent();
+            Print( u8"{\n" );
+            indent += 4;
+            printIndent();
+            s += 3;
+            skipSpaces();
+        }
+        else if( StartsWith( s, u8" }" ) )
+        {
+            Print( u8"\n" );
+            indent -= 4;
+            printIndent();
+            Print( u8"}\n" );
+            printIndent();
+            s += 2;
+            skipSpaces();
+        }
+        else if( StartsWith( s, u8"\\\"" ) )
+        {
+            Print( u8"\"" );
+            s += 2;
+        }
+        else
+        {
+            char8_t buf[ 2 ] = { *s, 0 };
+            Print( buf );
+            s++;
+        }
+    }
+} // end PrettyPrint()
+
 int main() // no C runtime, so no args here
 {
+    // I found that I could significantly shrink the program by switching from Unicode
+    // (wchar_t everywhere) to UTF8 (because that halved the size of all the string
+    // constants).
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
     g_hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
@@ -163,6 +269,7 @@ int main() // no C runtime, so no args here
     }
 
     bool whatIf = false;
+    bool whatIfRaw = false;
 
     cursor = SkipUntilWhitespace( cursor );
 
@@ -173,7 +280,14 @@ int main() // no C runtime, so no args here
     if( 0 == MyStrCmpI( installMode, u8"WhatIf" ) )
     {
         whatIf = true;
+    }
+    else if( 0 == MyStrCmpI( installMode, u8"WhatIfRaw" ) )
+    {
+        whatIfRaw = true;
+    }
 
+    if( whatIf || whatIfRaw )
+    {
         cursor = SkipUntilNotWhitespace( cursor );
 
         EXPECT_MORE_CMDLINE
@@ -214,13 +328,13 @@ int main() // no C runtime, so no args here
 
     char8_t* optionalArgs = cursor;
 
-    const char8_t c_Silent[] = u8"Silent";
-    const char8_t c_SilentWithProgress[] = u8"SilentWithProgress";
-    const char8_t c_Interactive[] = u8"Interactive";
+    const char8_t c_Silent[]                = u8"Silent";
+    const char8_t c_SilentWithProgress[]    = u8"SilentWithProgress";
+    const char8_t c_Interactive[]           = u8"Interactive";
 
-    bool bIsSilent = 0 == MyStrCmpI( installMode, c_Silent );
-    bool bIsSilentWithProgress = 0 == MyStrCmpI( installMode, c_SilentWithProgress );
-    bool bIsInteractive = 0 == MyStrCmpI( installMode, c_Interactive );
+    bool bIsSilent              = 0 == MyStrCmpI( installMode, c_Silent );
+    bool bIsSilentWithProgress  = 0 == MyStrCmpI( installMode, c_SilentWithProgress );
+    bool bIsInteractive         = 0 == MyStrCmpI( installMode, c_Interactive );
 
     if( !bIsSilent && !bIsSilentWithProgress && !bIsInteractive )
     {
@@ -234,14 +348,26 @@ int main() // no C runtime, so no args here
     char8_t* dst = newCmdLine;
     const char8_t* pastEnd = &newCmdLine[ _countof( newCmdLine ) ];
 
+    // N.B. Getting decent results from the PrettyPrint function depends on careful use of
+    // spaces around braces!
+    //
+    // If an opening brace is surrounded by space, or a closing brace is preceded by a
+    // space, that's the signal to the PrettyPrint function that we want the brace on its
+    // own line; else it's left alone (because it's a ScriptBlock)."
+
+    // Setting $env:PSModulePath to null forces a (machine-) default PSModulePath, which
+    // can be important to avoid "The PSModulePath Problem":
+    // https://github.com/PowerShell/PowerShell/issues/18108#issuecomment-2269703022
     const char8_t cmdFrag0[] = u8" -NoProfile -ExecutionPolicy RemoteSigned -Command $env:PSModulePath = $null ; "
                                u8"Set-StrictMode -Version Latest ; "
                                u8"try { "
                                    u8"$theScript = (iwr ";
 
+    // The utility of the environment variable here is to allow you to test a new version
+    // of your install script, without needing to touch your winget manifest.
     const char8_t cmdFrag1[] =     u8"$env:_POWERSHELL_STUB_TEST_URL_SUFFIX -UseBasic -EA Stop).Content ; "
                                    u8"$bytes = [System.Text.Encoding]::Unicode.GetBytes( $theScript ) ; "
-                                   u8"$hash = ([Security.Cryptography.SHA256]::Create().ComputeHash( $bytes ) | %{ $_.ToString('x2') }) -join '' ; "
+                                   u8"$hash = ([Security.Cryptography.SHA256]::Create().ComputeHash( $bytes ) | %{$_.ToString('x2')}) -join '' ; "
                                    u8"$expectedHash = '";
 
     const char8_t cmdFrag2[] =     u8"' ; "
@@ -254,9 +380,9 @@ int main() // no C runtime, so no args here
                                    u8"{ "
                                        u8"throw \\\"Signature error: $($sig.Status)\\\" "
                                    u8"} ; "
-                                   u8"Invoke-Expression \\\". { $theScript } -";
+                                   u8"Invoke-Expression \\\". {$theScript} -";
 
-    const char8_t cmdFrag3[] =     u8" -EA Stop\\\" ; "
+    const char8_t cmdFrag3[] =     u8" -EA Stop\\\" "
                                u8"} catch { ";
 
     const char8_t cmdFrag4_Interactive[] =
@@ -269,8 +395,13 @@ int main() // no C runtime, so no args here
     const char8_t cmdFrag4_Silent[] =
                                    u8"Write-Host $_ -Fore Red ; "
                                    u8"Start-Sleep -Seconds 5 ; " // Give people a chance to at least see the error.
-                                   u8"throw"
+                                   u8"throw "
                                u8"}";
+
+    if( !bIsInteractive )
+    {
+        dst = CopyStr( dst, u8" -NonInteractive", pastEnd );
+    }
 
     dst = CopyStr( dst, cmdFrag0, pastEnd );
     dst = CopyStr( dst, scriptUrl, pastEnd );
@@ -293,7 +424,15 @@ int main() // no C runtime, so no args here
 
     if( whatIf )
     {
-        Print( u8"\nWould run powershell.exe with command line:\n\n   " );
+        Print( u8"\nWould run powershell.exe with a command line to run the following code:\n" );
+        Print( u8"(use 'WhatIfRaw' instead to see the unmodified command line arguments)\n\n   " );
+        PrettyPrint( newCmdLine );
+        Print( u8"\n\n" );
+        ExitProcess( (UINT) -4 );
+    }
+    else if( whatIfRaw )
+    {
+        Print( u8"\nWould run powershell.exe with the following command line:\n\n   " );
         Print( newCmdLine );
         Print( u8"\n\n" );
         ExitProcess( (UINT) -4 );
@@ -301,11 +440,11 @@ int main() // no C runtime, so no args here
 
     if( dst == (pastEnd - 1) )
     {
-        Print( u8"Command line too long. Use WhatIf to see it.\n" );
+        Print( u8"Command line too long. Use WhatIf (or WhatIfRaw) to see it.\n" );
         ExitProcess( (UINT) -5 );
     }
 
-    PROCESS_INFORMATION pi = { };
+    PROCESS_INFORMATION pi = { 0 };
     STARTUPINFOA si;
     SecureZeroMemory( &si, sizeof( si ) );
     si.cb = (DWORD) sizeof( si );
@@ -325,6 +464,7 @@ int main() // no C runtime, so no args here
 
     if( !bItWorked )
     {
+        Print( u8"CreateProcess failed.\n" );
         ExitProcess( GetLastError() );
     }
 
@@ -339,6 +479,7 @@ int main() // no C runtime, so no args here
 
     if( !bItWorked )
     {
+        Print( u8"GetExitCodeProcess failed.\n" );
         ExitProcess( GetLastError() );
     }
 
